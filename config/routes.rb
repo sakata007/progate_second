6594acaae9a88c10442bb7541cde13ba047af868
sakata007 <be_best72@yahoo.co.jp>
2022-05-25 13:54:23 +0900

    progate stage 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'posts/index' => "posts#index"
   get "/" => "home#top"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "home/about" => "home#about"
+  get "about" => "home#about"
+  get "posts/new" => "posts#new"
+  get "posts/:id" => "posts#show"
+  post "posts/create" => "posts#create"
+  get "posts/:id/edit" => "posts#edit"
   # Defines the root path route ("/")
   # root "articles#index"
 end
