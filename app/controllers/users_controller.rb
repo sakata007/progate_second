9ac623426cebeb6e335903736ab129b08c424359
sakata007 <be_best72@yahoo.co.jp>
2022-06-05 11:10:53 +0900

    if_not_loaded

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(name:params[:name],email:params[:email])
+    @user = User.new(
+        name:params[:name],
+        email:params[:email],
+        password:params[:password])
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "ユーザー登録が完了しました"
       redirect_to("/users/#{@user.id}")
     else
@@ -30,6 +34,30 @@ class UsersController < ApplicationController
     @user.name = params[:name]
     @user.email = params[:email]
     @user.save
+  end
+
+  def login_form
+  end
+
+  def login
+    @user = User.find_by(email:params[:email],password:params[:password])
+
+    if @user
+      flash[:notice] = "ログインしました"
+      redirect_to("/posts/index")
+      session[:user_id] = @user.id
+    else
+      @error_message = "メールアドレスまたはパスワードが間違っています"
+      @email = params[:email]
+      @password = params[:password]
+      render("users/login_form")
+    end
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:notice] = "ログアウトしました"
+    redirect_to("/login")
   end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_26_063234) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_04_232116) do
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_26_063234) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password"
   end
 
 end
