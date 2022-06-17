8bae9b11caea2ebc58ee90217d59a4754fb05ffb
sakata007 <be_best72@yahoo.co.jp>
2022-06-17 14:13:40 +0900

    フォローした人の投稿のみ表示

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,23 +3,23 @@ class Post < ApplicationRecord
     validates :user_id, {presence: true}
 
     def user
-        return User.find_by(id:self.id)
+        return User.find_by(id:self.user_id)
     end
 
     # フォローしたときの処理
-    def follow(user_id)
-        relationships.create(follower_id: user_id)
-    end
+    # def follow(user_id)
+    #     relationships.create(follower_id: user_id)
+    # end
 
-    # フォローを外すときの処理
-    def unfollow(user_id)
-        relationships.find_by(follower_id: user_id).destroy
-    end
+    # # フォローを外すときの処理
+    # def unfollow(user_id)
+    #     relationships.find_by(follower_id: user_id).destroy
+    # end
 
-    # フォローしているか判定
-    def following?(user)
-        followings.include?(user)
-    end
+    # # フォローしているか判定
+    # def following?(user)
+    #     followings.include?(user)
+    # end
     
 
 end
