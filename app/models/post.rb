4be5929d769025ac2a6b74d719e2c2b621cd8277
sakata007 <be_best72@yahoo.co.jp>
2022-06-18 15:52:39 +0900

    commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
     validates :content, {presence: true, length: {maximum:140} }
     validates :user_id, {presence: true}
 
-    mount_uploader :image, :ImageUploader
+    # mount_uploader :image, :ImageUploader
 
     # 画像投稿実装のため追記
     #has_one_attached :image
