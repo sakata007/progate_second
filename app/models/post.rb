55ace7d325f5751decffe443addb0245915066d2
sakata007 <be_best72@yahoo.co.jp>
2022-06-18 21:58:25 +0900

    画像表示作業中

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
     validates :content, {presence: true, length: {maximum:140} }
     validates :user_id, {presence: true}
 
-    mount_uploader :image, :ImageUploader
+    # mount_uploader :image, :ImageUploader
 
     def user
         return User.find_by(id:self.user_id)
